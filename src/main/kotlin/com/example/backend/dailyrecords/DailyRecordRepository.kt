238e40f3ad44586ef3a307042d36f84137045a00
jm <mym1207@naver.com>
2026-02-09 23:28:09 +0900

    chore: Github Actions 추가

--- src/main/kotlin/com/example/backend/dailyrecords/DailyRecordRepository.kt
+++ src/main/kotlin/com/example/backend/dailyrecords/DailyRecordRepository.kt
@@ -4,6 +4,11 @@ import com.example.backend.user.User
 import com.linecorp.kotlinjdsl.support.spring.data.jpa.repository.KotlinJdslJpqlExecutor
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface DailyRecordRepository : JpaRepository<DailyRecord, Long>, KotlinJdslJpqlExecutor {
-    fun findByIdAndUser(id: Long, user: User): DailyRecord?
+interface DailyRecordRepository :
+    JpaRepository<DailyRecord, Long>,
+    KotlinJdslJpqlExecutor {
+    fun findByIdAndUser(
+        id: Long,
+        user: User,
+    ): DailyRecord?
 }
